dee3c15807fcadfcbdcb44f0b4e440028f6c09e8
osirisgrave <sergio.rivas1@gmail.com>
2016-01-03 10:39:58 -0600

    added pagination to pins index

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,7 +7,7 @@ class PinsController < ApplicationController
   #Create = New, Create; Read = index, show; Update = Edit, Update; Destroy = Destroy
 
   def index
-    @pins = Pin.all.order("created_at DESC")
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   end
 
   def show
